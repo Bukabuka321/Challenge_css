6f5900e34c55b9a34200822d5cf79b3f096f1a32
Bukabuka321 <20uva02@gmail.com>
2022-11-23 12:42:28 +0100

    used scss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import "./style.scss";
+import gordon from "./Images/gordon.jpeg";
 function App() {
+  const [input, setInput] = React.useState("");
+  const handleChange = (event) => setInput(event.target.value)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="container">
+      <div className="main_info">
+      <div className="photo">
+        <img src={gordon} alt="gordon"/>
+        </div> 
+      <div className="description">
+        <h1>Finally</h1>
+        <p>some good ***** food</p>
+        </div> 
+      </div> 
+      <div className="favorite">
+        <h2>Do you like it?</h2>
+        <form onSubmit={() => alert("thank you!")}>
+          <label htmlFor="is_favorite">Type here:</label>
+          <input  name="is_favorite" type='text' placeholder="Do you like it?" value={input} onChange={handleChange}>
+          </input>
+        </form>
+        <button type="submit" onClick={() => alert("thank you!")}>Submit</button>
+      </div>
+     </div>
     </div>
   );
 }
